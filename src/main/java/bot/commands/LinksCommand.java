fcee4a89ff6cd5665ce146be378e9bdb4f9a7d11
dokerplp <dokerplp2113@gmail.com>
2021-09-22 02:39:30 +0300

    version 0.0.6 (links)

--- src/main/java/bot/commands/LinksCommand.java
+++ src/main/java/bot/commands/LinksCommand.java
@@ -3,6 +3,7 @@ package bot.commands;
 import bot.run.AbitVTBot;
 import com.google.common.io.Resources;
 import com.sun.research.ws.wadl.Resource;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -19,10 +20,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.Reader;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class LinksCommand implements Command {
 
@@ -48,13 +46,15 @@ public class LinksCommand implements Command {
 
         URL file = Resources.getResource("links.json");
         try (
-                Reader reader = new FileReader(file.getFile())) {
-            JSONObject obj = (JSONObject) parser.parse(reader);
-            List<JSONObject> links = new LinkedList<>(Arrays.asList((JSONObject[]) obj.get("links")));
+                Reader reader = new FileReader(file.getFile()))
+        {
+            JSONArray links = (JSONArray) parser.parse(reader);
+            Iterator<JSONObject> iterator = links.iterator();
 
             List<List<InlineKeyboardButton>> buttons = new ArrayList<>();
 
-            for (JSONObject job : links){
+            while (iterator.hasNext()){
+                JSONObject job = iterator.next();
                 InlineKeyboardButton button = new InlineKeyboardButton();
                 button.setText((String) job.get("text"));
                 button.setUrl((String) job.get("link"));
